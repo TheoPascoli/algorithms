f74eaddafb29d6246ce4b6d450c229aabebf277a
Theo Pascoli <theo.pascoli@gmail.com>
2022-04-06 23:23:19 +0200

    Add top down merge sort

---
+++ src/com/pascoli/algorithms/sorting/Merge.java
@@ -0,0 +1,41 @@
+package com.pascoli.algorithms.sorting;
+
+import java.util.Scanner;
+
+public class Merge {
+
+    public static void sort(Comparable[] a) {
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (Comparable c : a) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] a = in.nextLine().split(" ");
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
